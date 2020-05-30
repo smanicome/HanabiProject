2ec20d8adeafa5b6955ab4872cccd3079bc85bdc
smanicome <s.manicome@gmail.com>
2020-05-30 12:45:37 +0200

    Player Update

--- src/fr/umlv/hanabi/Player.java
+++ src/fr/umlv/hanabi/Player.java
@@ -3,29 +3,56 @@ package fr.umlv.hanabi;
 import java.util.ArrayList;
 import java.util.Objects;
 
+/**
+ * Model of a player, basically a list of Card
+ */
 public class Player {
     private final ArrayList<Card> cards;
 
+    /**
+     * @param cards Initial hand (Must not be null)
+     */
     public Player(ArrayList<Card> cards) {
         this.cards = Objects.requireNonNull(cards);
     }
 
-    public void displayCards() {
-        StringBuilder builder = new StringBuilder();
-
-        for(Card card : cards) {
-            builder.append(card.toString());
-            builder.append(' ');
-        }
-
-        System.out.println(builder.toString());
-    }
-
+    /**
+     * @param i index of the card int player's hand
+     * @return card of given index in hand
+     * throws IllegalArgumentException if i exceeds boundaries
+     */
     public Card useCard(int i) {
         if (i < 0 || i > cards.size() - 1) {
             throw new IllegalArgumentException("Index out of bound");
         }
 
         return cards.remove(i);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Player{ ");
+        for(Card card : cards) {
+            builder.append(card.toString());
+            builder.append(' ');
+        }
+        builder.append("}");
+
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return Objects.equals(cards, player.cards);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cards);
     }
 }
